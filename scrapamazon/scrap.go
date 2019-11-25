d44bb1a37ae95f53aa643e18a63563e9cc0a950b
Bastian <jssb.2098@gmail.com>
2019-11-25 12:28:53 -0500

    add

---
+++ scrapamazon/scrap.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/gocolly/colly"
+)
+
+func main() {
+	c := colly.NewCollector()
+
+	c.OnRequest(func(r *colly.Request) {
+		fmt.Println("Visiting", r.URL)
+	})
+
+	c.OnHTML("div.s-result-list.s-search-results.sg-row", func(e *colly.HTMLElement) {
+		e.ForEach("div.a-section.a-spacing-medium", func(_ int, e *colly.HTMLElement) {
+			var productName string
+
+			productName = e.ChildText("span.a-size-medium.a-color-base.a-text-normal")
+
+			if productName == "" {
+				// If we can't get any name, we return and go directly to the next element
+				return
+			}
+
+			fmt.Printf("Product Name: %s \n", productName)
+		})
+	})
+
+	c.Visit("https://www.amazon.com/s?k=nintendo+switch&ref=nb_sb_noss_1")
+}
---
+++ scrapprgov/scrap.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/gocolly/colly"
+)
+
+func main() {
+
+	c := colly.NewCollector()
+
+	c.OnRequest(func(r *colly.Request) {
+		fmt.Println("Visitando ", r.URL)
+	})
+
+	c.OnHTML("div.cbq-layout-main",func (e *colly.HTMLElement){
+		e.ForEach("li.pageItemIndicator.listitem", func (_ int, e *colly.HTMLElement) {
+			var tendertittle string
+			tendertittle = e.ChildText("div.agency")
+			if tendertittle == ""{
+				return
+			}
+			fmt.Printf("Tittle: %s \n",tendertittle)
+		})
+	})
+
+	c.Visit("https://www2.pr.gov/subasta/Pages/subastas.aspx")
+
+}
