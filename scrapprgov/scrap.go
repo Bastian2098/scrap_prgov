16d5fd3b0e0c342048809db933f35101adf663cc
Bastian <jssb.2098@gmail.com>
2019-11-26 00:42:06 -0500

    added attributes

--- scrapprgov/scrap.go
+++ scrapprgov/scrap.go
@@ -26,12 +26,24 @@ func main() {
 
 	c.OnHTML("div.cbq-layout-main",func (e *colly.HTMLElement){
 		e.ForEach("li.pageItemIndicator.listitem", func (_ int, e *colly.HTMLElement) {
-			var tendertittle string
-			tendertittle = e.ChildText("span.title")
-			if tendertittle == ""{
+			var tittle,status,agency,location,opendate,preauctiondate,sheetdate string
+			tittle = e.ChildText("span.title")
+			status = e.ChildText("div.agency")
+			agency = e.ChildText("div.agency")
+			location = e.ChildText("div.localization")
+			opendate = e.ChildText("div.fechaApertura")
+			preauctiondate = e.ChildText("div.fechaPreSubasta")
+			sheetdate = e.ChildText("div.fechaPliegos")
+			if tittle == ""{
 				return
 			}
-			fmt.Printf("Tender tittle: %s \n",tendertittle)
+			fmt.Printf("Tender tittle: %s \n",tittle)
+			fmt.Printf(status)
+			fmt.Printf(agency)
+			fmt.Printf(location)
+			fmt.Printf(opendate)
+			fmt.Printf(preauctiondate)
+			fmt.Printf(sheetdate)
 		})
 	})
 
